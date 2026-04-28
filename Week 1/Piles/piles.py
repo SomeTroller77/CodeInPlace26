72617fa76135d68d0bd1a3c495f0b3933847ef74
saksham vitwekar <saksham.vitwekar@gmail.com>
2026-04-29 00:43:52 +0530

    Python files

---
+++ Week 1/Piles/piles.py
@@ -0,0 +1,31 @@
+from karel.stanfordkarel import *
+
+# File: piles.py
+# -----------------------------
+# The warmup program defines a "main"
+# function which should make Karel
+# pick up all the beepers in the world.
+def main():
+    move()
+    while beepers_present():
+        pick_beeper()
+    move()
+    while beepers_present():
+        pick_beeper()
+    move()
+    while beepers_present():
+        pick_beeper()
+    move()
+    move()
+    while beepers_present():
+        pick_beeper()
+    move()
+
+    
+   
+   
+   
+# don't edit these next two lines
+# they tell python to run your main function
+if __name__ == '__main__':
+    main()
